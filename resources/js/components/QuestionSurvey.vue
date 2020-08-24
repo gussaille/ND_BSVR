08d9d851679d5b6996d23ab6adf6ef62cbe48f81
Nico D <ndurocher@hotmail.fr>
2020-08-24 17:47:00 +0200

    Adding VueJs component Question

---
+++ resources/js/components/QuestionSurvey.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="question">
+        <h1>Sondage</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+	name: 'question-survey',
+    components:{},
+	data() {
+		return {
+		   
+		}
+	},
+	mounted() {
+	 
+	},
+	methods: {
+    
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
